26c669d252bdcafea082d2e539e0cfe69fd777ec
DavidJanLazar <54799260+DavidJanLazar@users.noreply.github.com>
2022-03-06 15:19:29 +0100

    Update streamlit_book.py

--- streamlit_book.py
+++ streamlit_book.py
@@ -39,6 +39,14 @@ cols_1 = st.columns((1, 1))
 
 book1 = list(dfdict[(dfdict["Book-Author"]==selectbox_author)&(dfdict["Book-Title"] ==selectbox_title)].iloc[:,2])
 
+
+if cols_1[0].button("Submit"):
+    if len(book1) != 0:
+        book1 = list(dfdict[(dfdict["Book-Author"]==selectbox_author)&(dfdict["Book-Title"] ==selectbox_title)].iloc[:,2])[0]
+    else:
+        st.write("There are no books satisfying your search!")
+
+        
 col_two_list_tit = listofbooks
 col_two_list_auth = listofauthors
 cols2 = st.columns((2))
@@ -47,16 +55,12 @@ selectbox_author2 = cols2[1].selectbox('Please choose the author', col_two_list_
 
 cols_2 = st.columns((1, 1))
 
-book2 = list(dfdict[(dfdict["Book-Author"]==selectbox_author2)&(dfdict["Book-Title"] ==selectbox_title2)].iloc[:,2])
-
-
-if cols_1[0].button("Submit"):
-    if len(book1) != 0:
-        book1 = list(dfdict[(dfdict["Book-Author"]==selectbox_author)&(dfdict["Book-Title"] ==selectbox_title)].iloc[:,2])[0]
-    else:
-        st.write("There are no books satisfying your search!")
+book2 = list(dfdict[(dfdict["Book-Author"]==selectbox_author2)&(dfdict["Book-Title"] ==selectbox_title2)].iloc[:,2])        
+        
+        
+        
+        
 if cols_2[0].button("Submit"):
-
     if len(book2) != 0:
         book2 = list(dfdict[(dfdict["Book-Author"]==selectbox_author2)&(dfdict["Book-Title"] ==selectbox_title2)].iloc[:,2])[0]
     else:
