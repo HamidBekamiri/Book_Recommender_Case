1aace21e7fd48b6defcafd5baf0436f5f309b018
DavidJanLazar <54799260+DavidJanLazar@users.noreply.github.com>
2022-03-06 15:13:00 +0100

    Update streamlit_book.py

--- streamlit_book.py
+++ streamlit_book.py
@@ -39,11 +39,29 @@ cols_2 = st.columns((1, 1))
 
 book1 = list(dfdict[(dfdict["Book-Author"]==selectbox_author)&(dfdict["Book-Title"] ==selectbox_title)].iloc[:,2])
 
+col_two_list_tit = listofbooks
+col_two_list_auth = listofauthors
+cols2 = st.columns((2))
+selectbox_title2 = cols2[0].selectbox('Please choose the book title', col_two_list_tit, index=0)
+selectbox_author2 = cols2[1].selectbox('Please choose the author', col_two_list_auth, index=0)
+
+#cols_2 = st.columns((1, 1))
+
+book2 = list(dfdict[(dfdict["Book-Author"]==selectbox_author2)&(dfdict["Book-Title"] ==selectbox_title2)].iloc[:,2])
+
+
 if cols_2[0].button("Submit"):
     if len(book1) != 0:
         book1 = list(dfdict[(dfdict["Book-Author"]==selectbox_author)&(dfdict["Book-Title"] ==selectbox_title)].iloc[:,2])[0]
     else:
         st.write("There are no books satisfying your search!")
+    if len(book2) != 0:
+        book2 = list(dfdict[(dfdict["Book-Author"]==selectbox_author2)&(dfdict["Book-Title"] ==selectbox_title2)].iloc[:,2])[0]
+    else:
+        st.write("There are no books satisfying your search!")    
+    
+    
+    
     #passing the book IDs to one list 
     listofproducts = [book1]
     #Making recommendation for books according to cosine similarity, passing the listofproducts to reommend
