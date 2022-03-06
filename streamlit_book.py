6fd8249447d384a412d758bfdc422042627f1949
DavidJanLazar <54799260+DavidJanLazar@users.noreply.github.com>
2022-03-06 15:23:13 +0100

    Update streamlit_book.py

--- streamlit_book.py
+++ streamlit_book.py
@@ -31,9 +31,9 @@ IDtoNameDict = dict(zip(list(dfdict.ProductId),list(dfdict["Book-Title"])))
 
 col_one_list_tit = listofbooks
 col_one_list_auth = listofauthors
-cols = st.columns((2))
-selectbox_title = cols[0].selectbox('Please choose the book title', col_one_list_tit, index=0)
-selectbox_author = cols[1].selectbox('Please choose the author', col_one_list_auth, index=0)
+#cols = st.columns((2))
+selectbox_title = st[0].selectbox('Please choose the book title', col_one_list_tit, index=0)
+selectbox_author = st[1].selectbox('Please choose the author', col_one_list_auth, index=0)
 
 cols_1 = st.columns((1, 1))
 
@@ -49,15 +49,13 @@ if cols_1[0].button("Submit"):
         
 col_two_list_tit = listofbooks
 col_two_list_auth = listofauthors
-cols2 = st.columns((2))
-selectbox_title2 = cols2[0].selectbox('Please choose the book title', col_two_list_tit, index=0)
-selectbox_author2 = cols2[1].selectbox('Please choose the author', col_two_list_auth, index=0)
+#cols2 = st.columns((2))
+selectbox_title2 = cols[0].selectbox('Please choose the book title', col_two_list_tit)
+selectbox_author2 = cols[1].selectbox('Please choose the author', col_two_list_auth, index=0)
 
 cols_2 = st.columns((1, 1))
 
 book2 = list(dfdict[(dfdict["Book-Author"]==selectbox_author2)&(dfdict["Book-Title"] ==selectbox_title2)].iloc[:,2])        
-        
-        
         
         
 if cols_2[0].button("Submit"):
