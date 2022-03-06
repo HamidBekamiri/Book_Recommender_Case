a424f60caec277f1f1069455a7422d7e694bd404
DavidJanLazar <54799260+DavidJanLazar@users.noreply.github.com>
2022-03-06 15:57:14 +0100

    Update streamlit_book.py

--- streamlit_book.py
+++ streamlit_book.py
@@ -42,6 +42,7 @@ book1 = list(dfdict[(dfdict["Book-Author"]==selectbox_author)&(dfdict["Book-Titl
 if cols_1[0].button("Submit",key="1"):
     if len(book1) != 0:
         item1 = list(dfdict[(dfdict["Book-Author"]==selectbox_author)&(dfdict["Book-Title"] ==selectbox_title)].iloc[:,2])[0]
+    return item1
     else:
         st.write("There are no books satisfying your search!")
 
@@ -59,6 +60,7 @@ book2 = list(dfdict[(dfdict["Book-Author"]==selectbox_author2)&(dfdict["Book-Tit
 if cols_2[0].button("Submit",key="2"):
     if len(book2) != 0:
         item2 = list(dfdict[(dfdict["Book-Author"]==selectbox_author2)&(dfdict["Book-Title"] ==selectbox_title2)].iloc[:,2])[0]
+    return item2        
     else:
         st.write("There are no books satisfying your search!")    
     
