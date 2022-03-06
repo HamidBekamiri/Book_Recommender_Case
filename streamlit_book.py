2dea1d36ad95d978512a36aeea9602b94b153c17
DavidJanLazar <54799260+DavidJanLazar@users.noreply.github.com>
2022-03-06 16:18:24 +0100

    Update streamlit_book.py

--- streamlit_book.py
+++ streamlit_book.py
@@ -9,7 +9,7 @@ from streamlit import caching
 
 st.set_page_config(page_title="Book Recommender System", page_icon="🐞", layout="centered")
 st.header("🐞 Book Recommender System!")
-st.subheader('Project for DataScentics entry case')
+st.subheader('This app will recommend you books based on what you read and liked')
 
 @st.cache 
 def loaddata():
@@ -29,6 +29,7 @@ model = tc.load_model("RS.model")
 df, dfdict, listofbooks, listofauthors = loaddata()
 IDtoNameDict = dict(zip(list(dfdict.ProductId),list(dfdict["Book-Title"])))
 
+#first book choosing
 col_one_list_tit = listofbooks
 col_one_list_auth = listofauthors
 selectbox_title = st.selectbox('Please choose the book title', col_one_list_tit, index=0,key="1")
@@ -45,7 +46,7 @@ if cols_1[0].button("Submit",key="1"):
     else:
         st.write("There are no books satisfying your search!")
 
-        
+ #second book choosing       
 col_two_list_tit = listofbooks
 col_two_list_auth = listofauthors
 selectbox_title2 = st.selectbox('Please choose the book title', col_two_list_tit, key="2")
@@ -61,10 +62,43 @@ if cols_2[0].button("Submit",key="2"):
         item2 = list(dfdict[(dfdict["Book-Author"]==selectbox_author2)&(dfdict["Book-Title"] ==selectbox_title2)].iloc[:,2])[0]
     else:
         st.write("There are no books satisfying your search!")  
+  
+#third book choosing       
+col_three_list_tit = listofbooks
+col_three_list_auth = listofauthors
+selectbox_title3 = st.selectbox('Please choose the book title', col_three_list_tit, key="3")
+selectbox_author3 = st.selectbox('Please choose the author', col_three_list_auth, index=0, key="3")
+
+cols_3 = st.columns((1, 1))
+
+book3 = list(dfdict[(dfdict["Book-Author"]==selectbox_author3)&(dfdict["Book-Title"] ==selectbox_title3)].iloc[:,2])        
         
+        
+if cols_3[0].button("Submit",key="3"):
+    if len(book3) != 0:
+        item3 = list(dfdict[(dfdict["Book-Author"]==selectbox_author3)&(dfdict["Book-Title"] ==selectbox_title3)].iloc[:,2])[0]
+    else:
+        st.write("There are no books satisfying your search!")
+    
+
     
     
-print(item1 + item2)    
+listofproducts = [item1, item2, item3]
+#Making recommendation for books according to cosine similarity, passing the listofproducts to reommend
+recommendation_item = model.get_similar_items(items=listofproducts, k=10)
+#Creating dataframe
+dfitem = pd.DataFrame(recommendation_item)
+#Data manipulation and transformation to show the top 10 books to recommend
+dfitem['item_occ'] = dfitem.groupby('similar').similar.transform('count')
+dfitem = dfitem.sort_values(["item_occ", "score"],ascending=(False,False))
+dfitem = dfitem[~dfitem["similar"].isin(listofproducts)]
+dfitem = dfitem.drop_duplicates(subset=['similar', "item_occ"])
+dfitem.index = range(len(dfitem))
+dfitem = dfitem.drop(columns=["ProductId", "score", "rank", "item_occ"])
+dfitem = dfitem.replace({"similar":IDtoNameDict})
+dfitem = dfitem.rename(columns={"similar":"recommended books"})
+st.write("These are the books you might be interested in, based on your previously liked books:")
+st.table(dfitem.head())
 
  
 
